f29d218f1f06726f3ab5ee130cb845432006343d
YiyiYi333 <skyyisky@gmail.com>
2025-10-20 22:17:20 +0800

    Site updated: 2025-10-20 22:17:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/10/18/hello-world/","2025/10/18/染/"];function toRandomPost(){
+var posts=["2025/10/20/伊伊伊/","2025/10/18/染/","2025/10/18/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
