a89eb6b941b32e55b2af04001d8365b368158176
YiyiYi333 <skyyisky@gmail.com>
2025-10-30 22:15:01 +0800

    Site updated: 2025-10-30 22:14:54

--- js/imgloaded.js
+++ js/imgloaded.js
@@ -78,9 +78,9 @@ const executeLoad = (config, target) => {
 };
 
 const config = {
-  smallSrc: '', // 小图链接 尽可能配置小于100k的图片
+  smallSrc: 'https://imgyi.ranranran.qzz.io/file/iuurX3Kh.png', // 小图链接 尽可能配置小于100k的图片
   largeSrc: 'https://imgyi.ranranran.qzz.io/file/lXObd1rI.png', // 大图链接 最终显示的图片
-  mobileSmallSrc: '/img/sjxt.jpg', // 手机端小图链接 尽可能配置小于100k的图片
+  mobileSmallSrc: 'https://imgyi.ranranran.qzz.io/file/iuurX3Kh.png', // 手机端小图链接 尽可能配置小于100k的图片
   mobileLargeSrc: 'https://imgyi.ranranran.qzz.io/file/lXObd1rI.png', // 手机端大图链接 最终显示的图片
   enableRoutes: ['/'],
   };
